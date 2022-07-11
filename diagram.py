781a58400cc8dda157664ff4e2f43e0afe674b0a
Jeffrey Huang <jeff0827@berkeley.edu>
2022-07-11 11:58:43 -0700

    able to save image of a graph

--- diagram.py
+++ diagram.py
@@ -1,11 +1,37 @@
 from tabnanny import verbose
+from pyxll import xl_func, plot
 import pandas as pd
 from openpyxl import load_workbook
+import matplotlib.pyplot as plt
+import xlsxwriter
+
+
+# Notes: If you specify sheet_name in read_excel it turns it into a dict, if not it will be a dataframe
 
 # Grab Dataframe
-df = pd.read_excel('test.xlsx', sheet_name=None)
+df = pd.read_excel('test.xlsx')
 
-# Plot the Dataframe - types = scatter, line, bar, plot.pie
-df.plot(x = 'COUNTRY', y = 'POP', type='bar')
+# Plot the Dataframe - kind  = scatter, line, bar, plot.pie
+# for plot have options of: title= , color={'line'='color'}, subplots = T/F, sharex= T/F, sharey = T/F, layout= (rows, columns), figize=(2,3), use_index=T/F, grid=T/F, legend = T/F, style=[]
 
+# Plots a bar graph
+ax = df.plot(x = 'COUNTRY', y = 'POP', kind ='bar')
 plt.show()
+
+fig = ax.get_figure()
+# sht.pictures.add(
+#     fig,
+#     name = "Pandas",
+#     update = True,
+#     left = sht.range("A21").left,
+#     top = sht.range("A21").top,
+#     height = 200,
+#     width = 300,
+# )
+
+plt.tight_layout()
+fig.savefig('graph.jpg', dpi=199)
+
+workbook = xlsxwriter.Workbook('test.xlsx')
+worksheet = workbook.add_worksheet()
+worksheet.insert_image('L2', 'graph.jpg')
